a46e67ba73200ad596a712a05079c6370050b2ab
glebreton <guillaume.le-breton@ens-lyon.fr>
2025-08-20 08:40:39 +0200

    update

--- to_creat_package.sh
+++ to_creat_package.sh
@@ -6,6 +6,12 @@ rm -r *.egg-info
 # creat the package
 python3.7 setup.py sdist
 mv dist/*.tar.gz alpaga.tar.gz 
+
+# clean up
+rm -r build
+rm -r dist
+rm -r *.egg-info
+
 # install the package
 python3.7 -m pip install alpaga.tar.gz
 
